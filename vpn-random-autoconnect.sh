d4219b0bf36eb1c32bc351e7a237c5752d9135cf
Yakir Kahana <YakirKahana@protonmail.com>
2021-03-13 16:38:01 +0200

    moved nmcli conncted check to the top
    to make sure all the vpn connections are loaded

--- vpn-random-autoconnect.sh
+++ vpn-random-autoconnect.sh
@@ -1,15 +1,15 @@
-#/bin/bash
+#!/bin/bash
+# This code derived from https://askubuntu.com/a/1033606
+# Originally by solsTiCe at askubuntu.com
+# userprofile: askubuntu.com/users/350004/solstice
+
+while ! nmcli dev status|grep -q connected; do
+    sleep 1
+done
+
 # get all VPN connections from nmcli
 conns=( $(nmcli con show | grep vpn |grep -o '^\S\+') )
 num_conns=${#conns[@]}
 random=$(($RANDOM % $num_conns))
-echo ${conns[$random]}
-
-# code by solsTiCe at askubuntu.com
-# userprofile: askubuntu.com/users/350004/solstice
-# taken from https://askubuntu.com/a/1033606
-while ! nmcli dev status|grep -q connected; do
-    sleep 1
-done
 exec nmcli con up ${conns[$random]}
 
