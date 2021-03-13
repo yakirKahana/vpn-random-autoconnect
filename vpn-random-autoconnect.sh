5fc2347317c26d16ad120ead55a908a391a6ff3e
Yakir Kahana <YakirKahana@protonmail.com>
2021-03-13 11:35:59 +0200

    added connection script by solsTiCe at askubuntu.com
    taken from https://askubuntu.com/questions/86365/how-can-i-have-my-vpn-connect-automatically-when-the-wireless-connects

--- vpn-random-autoconnect.sh
+++ vpn-random-autoconnect.sh
@@ -4,3 +4,12 @@ conns=( $(nmcli con show | grep vpn |grep -o '^\S\+') )
 num_conns=${#conns[@]}
 random=$(($RANDOM % $num_conns))
 echo ${conns[$random]}
+
+# code by solsTiCe at askubuntu.com
+# userprofile: askubuntu.com/users/350004/solstice
+# taken from https://askubuntu.com/questions/86365/how-can-i-have-my-vpn-connect-automatically-when-the-wireless-connects
+while ! nmcli dev status|grep -q connected; do
+    sleep 1
+done
+exec nmcli con up ${conns[$random]}
+
